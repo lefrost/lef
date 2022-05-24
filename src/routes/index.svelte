9c7dd8a287cef152c3c321b25974a3cce21bd09b
Lé <elcasaboxi@gmail.com>
2022-05-24 09:08:43 +0800

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,34 @@
+<div class="container  col--  col-middle--  landing">
+	<span>LÉFROST IS A</span>
+	<span><b>DESIGNER.</b></span>
+	<span><b>DEVELOPER.</b></span>
+	<span>& <b>FOUNDER.</b></span>
+	<br /><br /><br /><br />
+	<a href="https://twitter.com/lefrst" target="_blank" class="link">TWITTER</a>
+	<a href="https://github.com/lefrst" target="_blank" class="link">GITHUB</a>
+	<a href="/" class="link">LEF.LA</a>
+	<br /><br /><br /><br />
+	<a href="https://suaveseals.com" target="_blank" class="link"><b>@SUAVESEALS</b></a>
+</div>
+
+<style lang="scss">
+	@import '../assets/scss/all.scss';
+
+	.landing {
+		max-width: calc(100% - $wrapper-gutter * 2);
+		background-color: $black;
+		color: $white;
+		padding: 2em;
+		padding-right: 4em;
+
+		> span {
+			font-size: 3.5em;
+		}
+
+		> a {
+			color: $white;
+			font-size: 2.2em;
+			margin: 0.02em 0;
+		}
+	}
+</style>
