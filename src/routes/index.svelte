b9fe130adcc9b513a75af06aa0fbf9b9d14866ed
lé <elcasaboxi@gmail.com>
2023-10-25 06:48:29 +0800

    text twix

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,7 +18,7 @@
 		<a href="https://anilist.co/user/lefrost/mangalist" target="_blank" rel="noreferrer">manga</a>,
 		many hours of
 		gaming,
-		many hours of
+		many miles of
 		travel.
 		And in between, there's
 		<a href="https://github.com/lefrost" target="_blank" rel="noreferrer">code</a>,
@@ -32,8 +32,10 @@
 		<br /><br /><br /><br />
 		I'm on
 		<a href="https://discords.com/bio/p/lefrost" target="_blank" rel="noreferrer">Discord</a>
-		or
+		and
 		<a href="https://twitter.com/lefrost" target="_blank" rel="noreferrer">Twitter</a>,
+		<br /><br />
+		Updated October 2023.
 	</div>
 </div>
 
