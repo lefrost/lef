7280b23b98034c07ae07064a982e1ee706cd9168
lé <elcasaboxi@gmail.com>
2024-05-15 23:02:32 +0900

    css update and reviewsy prelim

--- src/routes/reviewsy/+page.svelte
+++ src/routes/reviewsy/+page.svelte
@@ -1,5 +1,131 @@
 <script lang="ts">
-  // tba: js
+  // imports
+	import { onDestroy, onMount } from 'svelte';
+  import * as utils from '../../assets/js/utils';
+  
+	// exports
+	// none
+  
+	// consts
+  
+  const TYPES = [
+    {
+      code: `cafe`,
+      name: `Cafe`,
+      sections: [
+        // tba
+      ]
+    },
+    {
+      code: `hotel`,
+      name: `Hotel`,
+      sections: [
+        {
+          name: `Overview`,
+          emoji: `🛏️`,
+          colour: `green`,
+          fragments: [
+            {
+              label: `One-liner`,
+              placeholder: `eg. great hotel north of ABC Station.`,
+              value: ``
+            },
+            {
+              label: `Staff`,
+              placeholder: `eg. staff was nice and professional.`,
+              value: ``
+            },
+            {
+              label: `General comments`,
+              placeholder: `eg. overall, good value for money.`,
+              value: ``
+            }
+          ]
+        },
+        {
+          name: `Service`,
+          emoji: `🛎️`,
+          colour: `yellow`,
+          fragments: [
+            // tba
+          ]
+        },
+        {
+          name: `Room`,
+          emoji: `🛋️`,
+          colour: `red`,
+          fragments: [
+            // tba
+          ]
+        },
+        {
+          name: `Breakfast`,
+          emoji: `🥞`,
+          colour: `orange`,
+          fragments: [
+            // tba
+          ]
+        },
+        {
+          name: `Location`,
+          emoji: `📍`,
+          colour: `blue`,
+          fragments: [
+            // tba
+          ]
+        },
+      ]
+    },
+    {
+      code: `konbini`,
+      name: `Konbini`,
+      sections: [
+        // tba
+      ]
+    }
+  ];
+
+  // vars
+
+  let selected_type = utils.clone(TYPES[0]);
+  let review_output = ``;
+
+  // dynamics
+  
+  $: if (selected_type) {
+    try {
+      // tba: update review output
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  // mount
+
+  onMount(() => {
+    try {
+      // tba
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  onDestroy(() => {
+    try {
+      // none
+    } catch (e) {
+      console.log(e);
+    }
+  });
+
+  // jobs
+  // none
+
+  // execs
+  // none
+
+  // funcs
+  // none
 </script>
 
 <!-- tba: html -->
