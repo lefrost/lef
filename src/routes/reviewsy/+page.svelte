aff1014d26fa13dbae0eef0e62f5c8b84eea8c12
lé <elcasaboxi@gmail.com>
2024-05-16 16:57:32 +0900

    reviewsy work consts

--- src/routes/reviewsy/+page.svelte
+++ src/routes/reviewsy/+page.svelte
@@ -13,7 +13,87 @@
       code: `cafe`,
       name: `Cafe`,
       sections: [
-        // tba
+        {
+          name: `Overview`,
+          emoji: `☕`,
+          colour: `green`,
+          fragments: [
+            {
+              label: `One-liner`,
+              placeholder: `eg. great cafe in ABC.`,
+              value: ``
+            },
+            {
+              label: `Drinks`,
+              placeholder: `eg. various types of great blend and straight coffee are served here.`,
+              value: ``
+            },
+            {
+              label: `Service`,
+              placeholder: `eg. service here is warm and friendly.`,
+              value: ``
+            },
+            {
+              label: `Aesthetic`,
+              placeholder: `eg. the lighting is tastefully dim, and jazz music is played in the background.`,
+              value: ``
+            }
+          ]
+        },
+        {
+          name: `Details`,
+          emoji: `🎯`,
+          colour: `red`,
+          fragmments: [
+            {
+              label: `Facade`,
+              placeholder: `eg. the cafe has a brick facade lined with greenery, easily spottable from the street.`,
+              value: ``
+            },
+            {
+              label: `Entrance`,
+              placeholder: `eg. the entrance is a wooden door to the right of a glass pane.`,
+              value: ``
+            },
+            {
+              label: `Layout`,
+              placeholder: `eg. the tables and bar are immediately visible upon entering.`,
+              value: ``
+            },
+            {
+              label: `Washroom`,
+              placeholder: `eg. the washroom is at the back of the space, through a small door.`,
+              value: ``
+            },
+            {
+              label: `Counter`,
+              placeholder: `eg. the counter is on the right when entering.`,
+              value: ``
+            },
+            {
+              label: `Solo`,
+              placeholder: `eg. there are several two-seater tables suitable if you're solo.`,
+              value: ``
+            },
+            {
+              label: `Smoking`,
+              placeholder: `eg. smoking is permitted.`,
+              value: ``
+            }
+          ]
+        },
+        {
+          name: `Recommendation`,
+          emoji: `✨`,
+          colour: `yellow`,
+          fragments: [
+            {
+              label: `Drinks and/or food`,
+              placeholder: `eg. any of the coffee choices are worth trying, but you can't go wrong with an Irish coffee here.`,
+              value: ``
+            } 
+          ]
+        }
       ]
     },
     {
@@ -27,7 +107,7 @@
           fragments: [
             {
               label: `One-liner`,
-              placeholder: `eg. great hotel north of ABC Station.`,
+              placeholder: `eg. great hotel in ABC.`,
               value: ``
             },
             {
@@ -47,7 +127,26 @@
           emoji: `🛎️`,
           colour: `yellow`,
           fragments: [
-            // tba
+            {
+              label: `Check-in/out time`,
+              placeholder: `eg. check-in begins at 1500, and check-out is until 1000.`,
+              value: ``
+            },
+            {
+              label: `Check-in/out system`,
+              placeholder: `eg. check-in/out is done at the front desk in the lobby.`,
+              value: ``
+            },
+            {
+              label: `Cleaning system`,
+              placeholder: `eg. room cleaning can be expected, and you paste a magnetic strip on your door to request it.`,
+              value: ``
+            },
+            {
+              label: `Cleaning time`,
+              placeholder: `eg. cleaning takes place between 1100 and 1400.`,
+              value: ``
+            },
           ]
         },
         {
@@ -55,7 +154,51 @@
           emoji: `🛋️`,
           colour: `red`,
           fragments: [
-            // tba
+            {
+              label: `General aesthetic`,
+              placeholder: `eg. simple but tasteful room design and furnishing.`,
+              value: ``
+            },
+            {
+              label: `Workspace`,
+              placeholder: `eg. there is a desk and chair suitable for working.`,
+              value: ``
+            },
+            {
+              label: `Air conditioning`,
+              placeholder: `eg. air conditioning works well, with responsive temperature control.`,
+              value: ``
+            },
+            {
+              label: `Window`,
+              placeholder: `eg. window can be opened for fresh air.`,
+              value: ``
+            },
+            {
+              label: `Lighting`,
+              placeholder: `eg. yellow lights in the room make for good ambiance.`,
+              value: ``
+            },
+            {
+              label: `Bathroom layout`,
+              placeholder: `eg. bathroom has a good and decently-sized layout.`,
+              value: ``
+            },
+            {
+              label: `Water heating`,
+              placeholder: `eg. water heating works well, and the tub's temperature control uses a convenient one-knob design.`,
+              value: ``
+            },
+            {
+              label: `Closet and hangers`,
+              placeholder: `eg. there's a small closet with three clothes hangers provided.`,
+              value: ``
+            },
+            {
+              label: `Amenities`,
+              placeholder: `eg. towels, pyjamas and bathroom amenities are provided in the room.`,
+              value: ``
+            }
           ]
         },
         {
@@ -63,7 +206,21 @@
           emoji: `🥞`,
           colour: `orange`,
           fragments: [
-            // tba
+            {
+              label: `Type and quality`,
+              placeholder: `eg. breakfast buffet is great, with a large spread.`,
+              value: ``
+            },
+            {
+              label: `Venue`,
+              placeholder: `eg. breakfast is held at the 12th floor.`,
+              value: ``
+            },
+            {
+              label: `System`,
+              placeholder: `eg. breakfast tickets are given at check-in, and you'll use one for each morning.`,
+              value: ``
+            }
           ]
         },
         {
@@ -71,7 +228,21 @@
           emoji: `📍`,
           colour: `blue`,
           fragments: [
-            // tba
+            {
+              label: `Transport`,
+              placeholder: `eg. ABC Station is a 5-minute walk away.`,
+              value: ``
+            },
+            {
+              label: `Shops`,
+              placeholder: `a 7-11 convenient shop is across the street.`,
+              value: ``
+            },
+            {
+              label: `Other places`,
+              placeholder: `eg. the XYZ Park is a 10-minute walk along the XYZ Lake, making for a nice walk.`,
+              value: ``
+            }
           ]
         },
       ]
@@ -80,7 +251,33 @@
       code: `konbini`,
       name: `Konbini`,
       sections: [
-        // tba
+        {
+          name: `Summary`,
+          emoji: ``,
+          colour: `green`,
+          fragments: [
+            {
+              label: `One-liner`,
+              placeholder: `eg. perfectly fine.`,
+              value: ``
+            },
+            {
+              label: `Convenience`,
+              placeholder: `eg. convenient when passing through ABC Station via the north exit.`,
+              value: ``
+            },
+            {
+              label: `Size and selection`,
+              placeholder: `eg. pretty small shop, but still has a decent selection.`,
+              value: ``
+            },
+            {
+              label: `Service`,
+              placeholder: `eg. staff at the counter was nice and professional.`,
+              value: ``
+            }
+          ]
+        }
       ]
     }
   ];
